77b7cfdcf961cc700900de6275f48e4fc55b7471
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-03-08 22:12:28 +0530

    Added node modules

---
+++ baray/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <button className='btn btn-outline-primary'>Baray</button>
+    </div>
+
+
+// ani mhane bootstrap :)
+  );
+}
+
+export default App;
